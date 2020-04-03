31706109a94d14d8725fbc508ae25bbc812b52b7
Rob Spears <rob.sp3ars@gmail.com>
2020-04-02 21:26:45 -0400

    Add argument parser, handle help, exit and unrecognized messages

--- slack_client.py
+++ slack_client.py
@@ -6,20 +6,29 @@ import os
 import slack
 import logging
 import re
+import random
+import argparse
+import sys
+import dotenv
+
+# exit program if not run in python3 environment
+if sys.version_info[0] < 3:
+    print('\n\tThis is a Python 3 program...\n')
+    sys.exit()
 
 
 class SlackClient:
 
     def __init__(self, bot_name, oauth_token):
+        self.logger = self.config_logger('slack_client', 'slack_client.log')
         self.token = oauth_token
         self.bot_name = bot_name
         self.bot_id = self.get_bot_id(bot_name)
-        self.logger = self.config_logger('slack_client', 'slack_client.log')
         self.rtm_client = slack.RTMClient(token=oauth_token)
         self.rtm_client.run_on(event='hello')(self.handle_hello)
         self.rtm_client.run_on(event='message')(self.handle_message)
 
-    def config_logger(self, logger_name, log_file, log_level='INFO'):
+    def config_logger(self, logger_name, log_file):
         '''
         Instantiates a logger that specifically logs information pertaining to
         a SlackClient instance
@@ -49,7 +58,7 @@ class SlackClient:
         logger.addHandler(file_handler)
 
         # set log level, defaults to 'INFO'
-        logger.setLevel(log_level)
+        logger.setLevel(os.environ['LOG_LVL'])
 
         return logger
 
@@ -79,11 +88,11 @@ class SlackClient:
         web_client = self.rtm_client._web_client
         assert web_client is not None
 
-        # web_client.chat_postMessage(
-        #     token=self.token,
-        #     channel='robs-test-channel',
-        #     text='I\'m Alive!'
-        # )
+        web_client.chat_postMessage(
+            token=self.token,
+            channel='robs-test-channel',
+            text='I\'m Alive!'
+        )
 
     def handle_message(self, **payload):
         '''
@@ -95,27 +104,169 @@ class SlackClient:
         Return:
             None
         '''
-        self.logger.info('Message recieved from RTM Client.')
+        self.logger.info('\'message\' event recieved from RTM Client.')
         web_client = self.rtm_client._web_client
         assert web_client is not None
         data = payload['data']
 
+        # check that payload data contains text, meaning it is a message
         if data.get('text', None):
+            self.logger.debug('Checking if message mentions bot')
             bot_id_regex = f'<@{self.bot_id}>'
             is_at_bot = re.search(bot_id_regex, data['text'])
+
             if is_at_bot:
-                web_client.chat_postMessage(
-                    token=self.token,
-                    channel=data['channel'],
-                    text=('I can hear you, but this is all ' +
-                          'I have to say for now')
-                )
+                # message mentions bot and user expects a response
+                self.logger.debug(
+                    'Message mentions bot, looking for appropriate response')
+                command = re.sub(bot_id_regex, '', data['text']).strip()
+
+                if command == 'help':
+                    # 'help' command
+                    self.logger.info(
+                        'Help command recieved, sending help message')
+                    self.send_help(
+                        web_client, data['channel'], ('Hi! Here\'s what' +
+                                                      ' I can do...'))
+                elif command == 'exit' or command == 'quit':
+                    self.logger.info(
+                        'Exit command received, exiting and sending ' +
+                        'exit message')
+                    self.handle_exit(web_client, data['channel'])
+
+                else:
+                    # unrecognized command
+                    self.logger.info(
+                        'Unrecognized command, showing help message')
+                    comments = [
+                        'You know I don\'t speak spanish...',
+                        'Nobody knows what it means, but it\'s provocative',
+                        'Aim for the bushes...',
+                        'I\'m a peacock, you gotta let me fly!',
+                        'Did I hear a \'niner\' in there?',
+                        'Maybe there\'s some sort of a translation problem...'
+                    ]
+                    rand_num = random.randint(0, len(comments) - 1)
+                    self.send_help(
+                        web_client, data['channel'], comments[rand_num])
+
+    def send_help(self, client, channel, message):
+        '''
+        posts a custom message followed by the help block
+
+        Parameters:
+            client --> the Slack WebClient instance
+            channel --> the channel to post the message
+            message --> the message to show above the help block
+
+        Return:
+            None
+        '''
+        self.logger.debug('Attempting to send help message')
+        client.chat_postMessage(
+            token=self.token,
+            channel=channel,
+            blocks=[
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f"{message}"
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": ("```Commands I understand:\n" +
+                                 "help -->\t\tShow this message\n" +
+                                 "ping -->\t\tShow uptime of this bot\n" +
+                                 "exit -->\t\tKill the bot\n" +
+                                 "quit -->\t\tSame as 'exit'\n" +
+                                 "list -->\t\tList current Twitter filters\n" +
+                                 "add  -->\t\tAdd some Twitter filters\n" +
+                                 "del  -->\t\tRemove some Twitter filters\n" +
+                                 "clear-->\t\tRemove all Twitter filters\n" +
+                                 "raise-->\t\tManual exception handler test```"
+                                 )
+                    }
+                }
+            ]
+        )
+        self.logger.info('Help message sent')
+
+    def handle_exit(self, client, channel):
+        '''
+        sends a message to the appropriate Slack channel and closes the
+        RTM Client connection
+
+        Parameters:
+            client --> the Slack Web Client to call Slack Web API
+            channel --> the channel to send the 'exit' message
+
+        Return:
+            None
+        '''
+        messages = [
+            'Adios!',
+            'Until next time!',
+            'Take it easy!',
+            'Au revoir!',
+            'Later alligator!'
+        ]
+        rand_num = random.randint(0, len(messages) - 1)
+        self.logger.debug('Attempting to send exit message')
+        client.chat_postMessage(
+            token=self.token,
+            channel=channel,
+            text=messages[rand_num]
+        )
+        self.logger.info('Exit message sent successfully')
+        self.rtm_client.stop()
+        self.logger.info('Closed RTM client connection')
 
 
-def main():
+def create_parser(args):
+    '''
+    creates an argument parser that handles command-line arguments
+
+    Parameters:
+        args --> arguments included on command line
+
+    Return:
+        an ArgParser instance
+    '''
+    parser = argparse.ArgumentParser(
+        description=('A Slackbot that interacts with the user and' +
+                     ' serves as a filter for a Twitter stream'),
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument(
+        '-l', '-log',
+        help=('integer that sets the log level for program log output' +
+              '\n0: DEBUG\n1: INFO(DEFAULT)\n2: WARNING' +
+              '\n3: ERROR\n4: CRITICAL'),
+        default=1,
+        metavar='',
+        dest='log_lvl'
+    )
+    return parser
+
+
+def main(args):
+    dotenv.load_dotenv('./.env')
+    parser = create_parser(args)
+    ns = parser.parse_args()
+    # set log level as environment variable
+    log_levels = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+    os.environ['LOG_LVL'] = log_levels[int(ns.log_lvl)]
+
     slack_bot = SlackClient('RobsTweetBot', os.environ['SLACK_TOKEN'])
     slack_bot.rtm_client.start()
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
